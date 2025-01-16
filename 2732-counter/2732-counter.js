16938568feb8e4bca90e00eb88221fa09b55d79e
sawad7007 <sawad7007@gmail.com>
2025-01-16 15:37:47 +0530

    Time: 42 ms (93.47%), Space: 48.3 MB (95%) - LeetHub

---
+++ 2732-counter/2732-counter.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number} n
+ * @return {Function} counter
+ */
+var createCounter = function(n) {
+    count=n;
+    return function() {
+        return count++
+    };
+};
+counter=createCounter(10);
+counter();
+
+/** 
+ * const counter = createCounter(10)
+ * counter() // 10
+ * counter() // 11
+ * counter() // 12
+ */
